c6ca41004edc1ead218eb0afff071d66f6122394
Rhenato Francisco Baraúna <rhbarauna@gmail.com>
2021-09-16 14:16:20 -0300

    first commit

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,34 @@
 import { Switch, Route, Redirect } from "react-router"
 import {Dashboard as DashboardPage, Home as HomePage, Login as LoginPage} from '../pages';
 import {Header} from '../components';
+import { Snackbar } from "@material-ui/core";
+import { useState } from "react";
 const ProtectedRoutes = () => {
-  <Switch>
-    <Route path='/' />
-  </Switch>
+  const [openSnackbar, setOpenSnackbar] = useState(false);
+  const [snackbarContent, setSnackbarContent] = useState('');
+  const handleCloseSnackbar = () => {}
+  
+  return (
+    <>
+     <Header />
+      <div className="container pageWrapper">
+        <Switch>
+          <Route path='/dashboard' component={DashboardPage}/>
+          <Route path='/messages' component={HomePage}/>
+          <Route path='/' exact>
+            <Redirect to="/messages" />
+          </Route>
+        </Switch>
+        <Snackbar
+          anchorOrigin={{ vertical:'top', horizontal:'top' }}
+          open={openSnackbar}
+          onClose={handleCloseSnackbar}
+          message={snackbarContent}
+          key={Math.random()}
+        />
+      </div>
+    </>
+  )
 }
 
 const Routes = () => {
@@ -13,16 +37,7 @@ const Routes = () => {
       <Switch>
         <Route path='/login' component={LoginPage}/>
         <Route path='/'>
-          <Header />
-          <div className="container pageWrapper">
-            <Switch>
-              <Route path='/dashboard' component={DashboardPage}/>
-              <Route path='/messages' component={HomePage}/>
-              <Route exact path='/'>
-                <Redirect to="/messages" />
-              </Route>
-            </Switch>
-          </div>
+          <ProtectedRoutes />          
         </Route>
       </Switch>
     </>
