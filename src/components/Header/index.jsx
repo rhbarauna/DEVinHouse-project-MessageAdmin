cd2ae14b74e0266dc92b03d86e4e7396b0ad3356
Rhenato Francisco Baraúna <rhbarauna@gmail.com>
2021-09-17 06:37:21 -0300

    header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,9 @@
-import { Box, Container } from '@material-ui/core';
-import { useHistory } from 'react-router';
+import { Box } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './index.css';
 
 const Header = () => {
-  const history = useHistory();
-  const navigate = (route) => {
-    history.push(route);
-  }
+  
   const routes = [
     { path: '/messages', label: 'Mensagens'},
     { path: '/dashboard', label: 'Dashboard'},
@@ -24,9 +20,13 @@ const Header = () => {
             {
               routes.map(
                 (route, idx) => (
-                  <li key={idx} onClick={()=>navigate(route.path)}>
+                  <Link
+                    component='li'
+                    key={idx}
+                    to={route.path}
+                  >
                     {route.label}
-                  </li>
+                  </Link>
                 )
               )
             }
